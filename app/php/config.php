7a64765c57e1a61400c5b8888b5483b494bd4802
SierikovRoman <rebit1love@gmail.com>
2017-07-25 21:42:07 +0300

    testing on heroku

---
+++ app/php/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+	$host='ec2-107-20-188-239.compute-1.amazonaws.com'; 
+	$port=5432;
+	$dbname='d9l72la37dr0q';
+	$user='jonkokiqpviszu';
+	$password='d51790cd312f0163d8fd08c8c3c13516d06b3a4d92c2933d11abde1d8d35394c';
+
+?>
