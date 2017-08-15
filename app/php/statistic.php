4b162a163efc8e959a73ee2a66ca263ff9a9b5f4
SierikovRoman <rebit1love@gmail.com>
2017-08-15 14:25:23 +0300

    testing Ip

--- app/php/statistic.php
+++ app/php/statistic.php
@@ -1,8 +1,24 @@
 <?php 
 
-$visitor_ip = $_SERVER['REMOTE_ADDR'];
-$date = date("d-m-y");
+// $visitor_ip = $_SERVER['REMOTE_ADDR'];
+// $date = date("d-m-y");
 
-echo $visitor_ip;
+function GetRealIp(){
+ if (!empty($_SERVER['HTTP_CLIENT_IP']))
+ {
+   $ip=$_SERVER['HTTP_CLIENT_IP'];
+ }
+ elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+ {
+  $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+ }
+ else
+ {
+   $ip=$_SERVER['REMOTE_ADDR'];
+ }
+ return $ip;
+}
+
+echo GetRealIp();
 
 ?>
